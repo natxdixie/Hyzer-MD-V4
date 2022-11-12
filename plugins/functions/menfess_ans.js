1a473572ac693649b82dd2fd146dc267819174e7
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-11-12 21:10:50 +0700

    :v

---
+++ plugins/functions/menfess_ans.js
@@ -0,0 +1,9 @@
+var handler = async (msg, { client }) => {
+client.menfess = client.menfess ? client.menfess : {}
+if (client.menfess[msg.sender].id != 0) {
+msg.reply("🚩 Reply pesan menfess yang dikirim bot untuk membalas menfess.")
+} else return false
+}
+handler.command = /^(balasmenfess)$/i
+
+module.exports = handler
