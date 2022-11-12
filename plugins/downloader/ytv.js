497b1ebf3e5479e1e3fe9b82c223d7b4ed63f9ab
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-11-12 08:40:45 +0700

    + simple

--- plugins/downloader/ytv.js
+++ plugins/downloader/ytv.js
@@ -2,9 +2,9 @@ var handler = async (msg, {
 client, text, usedPrefix, command 
 }) => {
 if (!text) return msg.reply(`🚩 Gunakan format ${usedPrefix + command} *<link-youtube>*\n\n*Contoh :* ${usedPrefix + command} https://youtu.be/Db3QI5OUO5o`)
-require('caliph-api').downloader.youtube.ytmp4(text).then(async v => {
-const vk = await client.sendImage(msg.from, v.thumb, `*乂 Y O U T U B E - V I D E O*\n\n   *◦ Title :* ${v.title}\n   *◦ Quality :* ${v.quality}\n   *◦ Size :* ${v.size}\n   *◦ Duration :* ${v.duration}\n   *◦ Views :* ${v.views}\n   *◦ Like :* ${v.like}\n   *◦ Dislike :* ${v.dislike}\n   *◦ Channel :* ${v.channel}\n   *◦ Upload  :* ${v.uploadDate}\n   *◦ Description :* ${v.desc}`, msg, { isUrl:true })
-client.sendVideo(msg.from, v.result, null, vk, { isUrl:true })
+client.sendReact(msg.from, '🕒', msg.key)
+require('caliph-api').downloader.yt.mp4(text).then(async v => {
+client.sendVideo(msg.from, v.result.result, `*乂 Y O U T U B E - V I D E O*\n\n   *◦ Title :* ${v.result.title}\n   *◦ Quality :* ${v.result.quality}\n   *◦ Size :* ${v.result.size}\n   *◦ Duration :* ${v.result.duration}\n   *◦ Channel :* ${v.result.channel}`, msg, { isUrl:true })
 })
 }
 handler.help = ['ytv','ytmp4']
