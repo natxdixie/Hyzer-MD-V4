1e4b5185cbf2f5b1125503faa4b6d0299d701c37
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-11-13 10:26:17 +0700

    +

---
+++ plugins/tools/emojimix.js
@@ -0,0 +1,20 @@
+var handler = async(msg, { 
+client, text, usedPrefix, command 
+}) => {
+if (!text) return msg.reply(`🚩 Gunakan format ${usedPrefix + command} emoji1+emoji2\n\n*Contoh :* ${usedPrefix + command} 😁+😘`)
+var [emo1, emo2] = text.split`+`
+if (emo1.length > 1) return msg.reply(`🚩 Gunakan format ${usedPrefix + command} emoji1+emoji2\n\n*Contoh :* ${usedPrefix + command} 😁+😘`)
+if (emo2.length > 1) return msg.reply(`🚩 Gunakan format ${usedPrefix + command} emoji1+emoji2\n\n*Contoh :* ${usedPrefix + command} 😁+😘`)
+try {
+Func.fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emo1)}_${encodeURIComponent(emo2)}`).then(async v => { 
+client.sendImageAsSticker(msg.from, v.results[0].url, msg, { packname: Info.stickerpack, author: `ig : ${Info.instagram}` })
+})
+} catch(e) {
+msg.reply('🚩 Emoji Invalid.')
+}
+}
+handler.help = ['emojimix']
+handler.tags = ['tools']
+handler.command = /^(emojimix)$/i
+
+module.exports = handler
