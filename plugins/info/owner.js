0589382fabf5b007b8bb891850cb313e1b8e8b22
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-11-16 02:36:16 +0700

    Update owner.js

--- plugins/info/owner.js
+++ plugins/info/owner.js
@@ -1,7 +1,7 @@
 var handler = async(msg, { 
 client 
 }) => {
-var sendOwner = await client.sendContact(msg.from, Info.owner, msg)
+var sendOwner = await sendKontak(msg.from, Info.owner, msg)
 var teks = `*🚩 Hi kak @${msg.sender.split('@')[0]} ini adalah kontak creator kami, kami tidak akan menanggapi hal yang tidak penting.*`
 client.sendMessage(msg.from, { text: teks, mentions: client.parseMention(teks)}, { quoted:sendOwner })
 }
@@ -10,3 +10,15 @@ handler.tags = ['info']
 handler.command = /^(owner|creator)$/i
 
 module.exports = handler
+
+// by me
+async function sendKontak(jid, kon, quoted = '', opts = {}) {
+    let list = []
+    for (let i of kon) {
+    list.push({
+    displayName: await client.getName(i + '@s.whatsapp.net'),
+    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await client.getName(i + '@s.whatsapp.net')}\nFN:${await client.getName(i + '@s.whatsapp.net')}\nitem1.TEL;waid=${i}:${i}\nitem1.X-ABLabel:Ponsel\nitem2.EMAIL;type=INTERNET:${Info.gmail}\nitem2.X-ABLabel:Email\nitem3.URL:${Info.instagram}\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Indonesia;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`
+    })
+    }
+    return client.sendMessage(jid, { contacts: { displayName: `${list.length} Kontak`, contacts: list }, ...opts }, { quoted })
+}
